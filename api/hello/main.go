e159e30c480ba286db79c8f127fe4cb3253ccb7f
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-07-26 12:41:50 -0700

    Update main.go

--- api/hello/main.go
+++ api/hello/main.go
@@ -1,1 +1,34 @@
+package main
 
+import (
+"log"
+"net/http"
+)
+func home(w http.ResponseWriter, r *http.Request) {
+w.Write([]byte("Hello from Snippetbox"))
+}
+// Add a snippetView handler function.
+func snippetView(w http.ResponseWriter, r *http.Request) {
+w.Write([]byte("Display a specific snippet...
+"))
+}
+// Add a snippetCreate handler function.
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+w.Write([]byte("Display a form for creating a new snippet...
+"))
+}
+func main() {
+// Register the two new handler functions and corresponding route patterns with
+// the servemux, in exactly the same way that we did before.
+mux := http.NewServeMux()
+mux.HandleFunc("/"
+, home)
+mux.HandleFunc("/snippet/view"
+mux.HandleFunc("/snippet/create"
+, snippetView)
+, snippetCreate)
+log.Print("starting server on :4000")
+err := http.ListenAndServe(":4000"
+log.Fatal(err)
+, mux)
+}
